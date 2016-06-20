7ff20412b9e31e2c9479bcb7a52ddf25fb5f929e
shijie.xiang <mike@ptthink.com>
2016-06-20 18:33:21 +0800

    First commit

---
+++ src/util/ecb_datetime.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Russell-X-Shanso
+%%% @copyright (C) 2016, Russell-X-Shanso
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2016.06.20   18:28
+%%%-------------------------------------------------------------------
+-module(ecb_datetime).
+-author("Russell-X-Shanso").
+
+%% API
+-export([]).
